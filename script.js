7a75b733252ec6b76c0db855f522a9d9eb33936a
Thirdii <ferdinandmiguelvillanueva@gmail.com>
2025-01-28 01:09:58 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -57,6 +57,11 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbw3yAINb1i5SsPYm25YeN
 
 //graphics card
 
-
+function scrollToTop() {
+    window.scrollTo({
+      top: 0, 
+      behavior: 'smooth' 
+    });
+  }
 
 
